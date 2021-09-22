c4274c04a0adb374df7b40cd59ec5208cd0ff348
Marcos Cordeiro <marcos.cordeiro@andbank.com.br>
2021-09-22 14:40:34 -0300

    Better KonsoleSink formatting

--- CrunchyDownloader/KonsoleSink.cs
+++ CrunchyDownloader/KonsoleSink.cs
@@ -13,7 +13,7 @@ namespace CrunchyDownloader
         {
             var width = System.Console.WindowWidth;
             var height = System.Console.WindowHeight / 2 - 1;
-            
+
             Console = Window.OpenBox("Logs", width, height, new BoxStyle()
             {
                 ThickNess = LineThickNess.Single,
@@ -21,10 +21,38 @@ namespace CrunchyDownloader
             });
         }
 
+        private static ConsoleColor GetColor(LogEventLevel level) =>
+            level switch
+            {
+                LogEventLevel.Verbose => ConsoleColor.DarkGray,
+                LogEventLevel.Debug => ConsoleColor.DarkGray,
+                LogEventLevel.Information => ConsoleColor.Blue,
+                LogEventLevel.Warning => ConsoleColor.Yellow,
+                LogEventLevel.Error => ConsoleColor.Red,
+                LogEventLevel.Fatal => ConsoleColor.Red,
+                _ => throw new ArgumentOutOfRangeException(nameof(level), level, null)
+            };
+
+        private static string GetShortLevel(LogEventLevel level) => level switch
+        {
+            LogEventLevel.Verbose => "VER",
+            LogEventLevel.Debug => "DBG",
+            LogEventLevel.Information => "INF",
+            LogEventLevel.Warning => "WRN",
+            LogEventLevel.Error => "ERR",
+            LogEventLevel.Fatal => "FAT",
+            _ => throw new ArgumentOutOfRangeException(nameof(level), level, null)
+        };
+
         public void Emit(LogEvent logEvent)
         {
-            var renderMessage = logEvent.RenderMessage(null);
-            Console.WriteLine(renderMessage);
+            var level = GetShortLevel(logEvent.Level);
+            var renderMessage = logEvent.RenderMessage();
+            Console.Write("[");
+            Console.Write(GetColor(logEvent.Level), level);
+            Console.Write("] ");
+            Console.Write(renderMessage);
+            Console.Write(Environment.NewLine);
         }
     }
 }
