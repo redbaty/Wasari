ca43b5778babf97358faa032f8402cc8528176ce
Marcos Cordeiro <marcos.cordeiro@andbank.com.br>
2021-09-22 14:37:20 -0300

    Adds StringExtensions to help parse stdout

---
+++ CrunchyDownloader/Extensions/StringExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace CrunchyDownloader.Extensions
+{
+    internal static class StringExtensions
+    {
+        public static bool GetValueFromRegex<T>(this string input, string regex, out T @out)
+        {
+            var canParseFromRegex = GetValueFromRegex(input, regex, out var value);
+
+            if (canParseFromRegex)
+            {
+                if (typeof(T) == typeof(double))
+                {
+                    if (double.TryParse(value, NumberStyles.Any, CultureInfo.InvariantCulture, out var d))
+                    {
+                        @out = (T)Convert.ChangeType(d, typeof(T));
+                        return true;
+                    }
+                }
+                else if (typeof(T) == typeof(int))
+                {
+                    if (int.TryParse(value, NumberStyles.Any, null, out var i))
+                    {
+                        @out = (T)Convert.ChangeType(i, typeof(T));
+                        return true;
+                    }
+                }
+                else if (typeof(T) == typeof(string))
+                {
+                    @out = (T)Convert.ChangeType(value, typeof(T));
+                    return true;
+                }
+            }
+
+            @out = default;
+            return false;
+        }
+
+        private static bool GetValueFromRegex(this string input, string regex, out string @out)
+        {
+            if (!string.IsNullOrEmpty(input) && Regex.Match(input, regex) is { Success: true } match)
+            {
+                @out = match.Groups[1].Value;
+                return true;
+            }
+
+            @out = null;
+            return false;
+        }
+    }
+}
