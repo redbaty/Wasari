d646a2d36f665dff1837939ef7f1ff37bf97fff1
Marcos Cordeiro <marcos.cordeiro@andbank.com.br>
2021-09-22 14:40:45 -0300

    Adds rolling file

--- CrunchyDownloader/Program.cs
+++ CrunchyDownloader/Program.cs
@@ -1,10 +1,13 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
 using CliFx;
 using CrunchyDownloader.App;
 using CrunchyDownloader.Commands;
 using Microsoft.Extensions.DependencyInjection;
 using PuppeteerSharp;
 using Serilog;
+using Serilog.Events;
 
 namespace CrunchyDownloader
 {
@@ -12,9 +15,12 @@ namespace CrunchyDownloader
     {
         private static async Task<int> Main()
         {
+            Console.CursorVisible = false;
             Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Debug()
-                .WriteTo.Sink<KonsoleSink>()
+                .WriteTo.Sink<KonsoleSink>(LogEventLevel.Information)
+                .WriteTo.File(
+                    Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                        "CrunchyDownloader", "logs", "log.txt"), rollingInterval: RollingInterval.Day, restrictedToMinimumLevel: LogEventLevel.Verbose)
                 .CreateLogger();
 
             var browserFetcher = new BrowserFetcher();
