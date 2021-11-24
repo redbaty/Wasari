40a502151ac3775622b27bf0f9d0d0e20f4c3f42
Marcos Cordeiro <marcos.cordeiro@andbank.com.br>
2021-11-24 01:30:08 -0300

    Rework episode/season parsing (via API)

--- Wasari.Crunchyroll/CrunchyRollService.cs
+++ Wasari.Crunchyroll/CrunchyRollService.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Crunchyroll.API.Models;
 using Flurl;
 using Microsoft.Extensions.Logging;
 using PuppeteerSharp;
 using Wasari.Abstractions;
 using Wasari.Crunchyroll.Abstractions;
+using Wasari.Crunchyroll.API;
+using Wasari.Crunchyroll.Models;
 using Wasari.Puppeteer;
 
-namespace Crunchyroll.API
+namespace Wasari.Crunchyroll
 {
     public class CrunchyRollService : ISeriesProvider<CrunchyrollSeasonsInfo>
     {
@@ -25,59 +26,44 @@ namespace Crunchyroll.API
         private ILogger<CrunchyRollService> Logger { get; }
 
         private Browser Browser { get; }
-
-        private static string[] BannedKeywords = { "(Russian)", "Dub)" };
-
+        
         private CrunchyrollApiService CrunchyrollApiService { get; }
 
         private async IAsyncEnumerable<CrunchyrollSeasonsInfo> GetSeasonsInfo(Page seriesPage,
-            IReadOnlyDictionary<string, ApiEpisode> episodesDictionary)
+            IReadOnlyDictionary<string, ApiEpisode> episodesDictionary, string seriesId)
         {
-            var seasonsHandles = await seriesPage.XPathAsync("//ul[@class='list-of-seasons cf']/li/a");
-            var seasonNumber = 0;
+            var seasons = await CrunchyrollApiService.GetSeasons(seriesId).ToDictionaryAsync(i => i.Id);
+            var episodes = await GetEpisodes(seriesPage, episodesDictionary).ToArrayAsync();
 
-            Logger.LogDebug("{@Handles} seasons handles found", seasonsHandles.Length);
-
-            if (seasonsHandles.Length != 0)
+            foreach (var episodesGroupedBySeason in episodes.GroupBy(i => i.SeasonId))
             {
-                foreach (var seasonHandle in seasonsHandles.Reverse())
+                var season = seasons[episodesGroupedBySeason.Key];
+                
+                if(season.IsDubbed)
+                    continue;
+                
+                var seasonsInfo = new CrunchyrollSeasonsInfo
                 {
-                    await using var realSeasonHandle = await seasonHandle.SingleOrDefaultXPathAsync("./..");
-                    var title = await seasonHandle.GetPropertyValue<string>("title");
-                    var trimmedTitle = title.Trim();
-
-                    if (BannedKeywords.Any(i => trimmedTitle.EndsWith(i)))
-                    {
-                        Logger.LogWarning("Ignoring season due to blacklisted words {@SeasonName}", trimmedTitle);
-                        continue;
-                    }
-
-                    Logger.LogDebug("Returning season {@SeasonNumber} '{@SeasonTile}'", seasonNumber + 1, trimmedTitle);
-                    var seasonsInfo = new CrunchyrollSeasonsInfo
-                    {
-                        Season = seasonNumber + 1,
-                        Title = trimmedTitle,
-                        Episodes = new List<CrunchyrollEpisodeInfo>()
-                    };
-
-                    await foreach (var episodeInfo in GetEpisodes(realSeasonHandle, episodesDictionary, seasonsInfo))
-                    {
-                        seasonsInfo.Episodes.Add(episodeInfo);
-                    }
-
-                    yield return seasonsInfo;
-                    seasonNumber++;
-                }
-            }
-            else
-            {
-                Logger.LogDebug("No seasons found, returning a default one");
-                yield return new CrunchyrollSeasonsInfo
-                {
-                    Season = 1,
-                    Title = null,
-                    Episodes = Array.Empty<CrunchyrollEpisodeInfo>()
+                    Season = season.Number,
+                    Id = season.Id,
+                    Title = season.Title,
+                    Episodes = new List<CrunchyrollEpisodeInfo>()
                 };
+
+                foreach (var crunchyrollEpisodeInfo in episodesGroupedBySeason.Select((i, index) =>
+                             new CrunchyrollEpisodeInfo
+                             {
+                                 Id = i.Id,
+                                 Name = i.Name,
+                                 Special = i.Special,
+                                 Url = i.Url,
+                                 ThumbnailId = i.ThumbnailId,
+                                 Number = i.Number?.ToString("00"),
+                                 SequenceNumber = i.Number ?? index,
+                                 SeasonInfo = seasonsInfo
+                             })) seasonsInfo.Episodes.Add(crunchyrollEpisodeInfo);
+
+                yield return seasonsInfo;
             }
         }
 
@@ -105,7 +91,7 @@ namespace Crunchyroll.API
             var episodesDictionary = await CrunchyrollApiService.GetAllEpisodes(id)
                 .ToDictionaryAsync(i => i.ThumbnailIds.Single());
 
-            var seasons = await GetSeasonsInfo(seriesPage, episodesDictionary).ToListAsync();
+            var seasons = await GetSeasonsInfo(seriesPage, episodesDictionary, id).ToListAsync();
             var specialSeason = CreateSpecialSeason(seasons);
 
             if (specialSeason.Episodes.Any())
@@ -136,7 +122,9 @@ namespace Crunchyroll.API
 
                     foreach (var specialEpisode in specialEpisodes)
                     {
-                        var currentEpisode = specialSeason.Episodes.Any() ? specialSeason.Episodes.Max(o => o.SequenceNumber) : -1;
+                        var currentEpisode = specialSeason.Episodes.Any()
+                            ? specialSeason.Episodes.Max(o => o.SequenceNumber)
+                            : -1;
                         var newEpisodeNumber = currentEpisode + 1;
                         var convertedSpecialEpisode = new CrunchyrollEpisodeInfo
                         {
@@ -149,7 +137,7 @@ namespace Crunchyroll.API
                             SequenceNumber = newEpisodeNumber,
                             ThumbnailId = specialEpisode.ThumbnailId
                         };
-                        
+
                         specialSeason.Episodes.Add(convertedSpecialEpisode);
                         seasonInfo.Episodes.Remove(specialEpisode);
                     }
@@ -159,67 +147,39 @@ namespace Crunchyroll.API
             return specialSeason;
         }
 
-        private async IAsyncEnumerable<CrunchyrollEpisodeInfo> GetEpisodes(ElementHandle seriesHandle,
-            IReadOnlyDictionary<string, ApiEpisode> episodesDictionary, CrunchyrollSeasonsInfo seasonsInfo)
+        private async IAsyncEnumerable<RawCrunchyrollEpisode> GetEpisodes(Page pageHandle,
+            IReadOnlyDictionary<string, ApiEpisode> episodesDictionary)
         {
             var episodesHandles =
-                await seriesHandle.XPathAsync(".//a[@class='portrait-element block-link titlefix episode']");
-            var lastEpisode = 0;
+                await pageHandle.XPathAsync("//a[@class='portrait-element block-link titlefix episode']");
 
-            foreach (var episodeHandle in episodesHandles.Reverse())
+            foreach (var episodeHandle in episodesHandles)
             {
-                await using var seasonHandle = await episodeHandle.SingleOrDefaultXPathAsync("./../../../..");
-                await using var seasonTitleHandle =
-                    seasonHandle != null ? await seasonHandle.SingleOrDefaultXPathAsync("./a") : null;
-                var seasonTitle = seasonTitleHandle != null
-                    ? await seasonTitleHandle.GetPropertyValue<string>("title")
-                    : string.Empty;
                 await using var imageHandle = await episodeHandle.SingleOrDefaultXPathAsync("./img");
                 await using var srcHandle = await imageHandle.SingleOrDefaultXPathAsync("./@data-thumbnailurl") ??
                                             await imageHandle.SingleOrDefaultXPathAsync("./@src");
                 var srcValue = await srcHandle.GetPropertyValue<string>("value");
-                var name = await imageHandle.GetPropertyValue<string>("alt");
-
                 var thumbnailUrl = Url.ParsePathSegments(srcValue).LastOrDefault();
                 var thumbnailId = thumbnailUrl?[..32];
                 var apiEpisode = episodesDictionary.GetValueOrDefault(thumbnailId);
-
-                if (apiEpisode != null && string.IsNullOrEmpty(seasonsInfo.Id))
-                {
-                    seasonsInfo.Id = apiEpisode.SeasonId;
-                }
-
                 var url = await episodeHandle.GetPropertyValue<string>("href");
-                var episodeUrl = url[url.LastIndexOf("/", StringComparison.Ordinal)..];
-                var episode = episodeUrl.Split('-').Skip(1).First();
 
-                if (BannedKeywords.Any(i => seasonTitle.EndsWith(i)))
+                if (apiEpisode == null)
                 {
-                    Logger.LogWarning("Ignoring episode due to blacklisted words {@EpisodeName} {@SeasonTitle}", name,
-                        seasonTitle);
+                    Logger.LogWarning("Failed to match episode to API. {@Url}", url);
                     continue;
                 }
-
-                var episodeNumber = int.TryParse(episode, out var parsedEpisodeNumber)
-                    ? parsedEpisodeNumber
-                    : lastEpisode;
-
-                if (apiEpisode is { IsClip: true })
-                    continue;
-
-                yield return new CrunchyrollEpisodeInfo
+                
+                yield return new RawCrunchyrollEpisode
                 {
-                    Name = name,
+                    Id = apiEpisode.Id,
+                    Name = apiEpisode.Title,
+                    Special = !apiEpisode.EpisodeNumber.HasValue,
                     Url = url,
-                    SequenceNumber = apiEpisode?.SequenceNumber ?? episodeNumber,
-                    ThumbnailId = thumbnailId,
-                    Number = apiEpisode?.SequenceNumber.ToString("00") ?? episodeNumber.ToString("00"),
-                    Id = apiEpisode?.Id,
-                    SeasonInfo = seasonsInfo,
-                    Special = !apiEpisode?.EpisodeNumber.HasValue ?? false
+                    SeasonId = apiEpisode.SeasonId,
+                    SeasonNumber = apiEpisode.SeasonNumber,
+                    Number = apiEpisode.EpisodeNumber ?? apiEpisode.SequenceNumber
                 };
-
-                lastEpisode++;
             }
         }
     }
