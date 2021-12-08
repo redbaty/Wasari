26470f3288de0664860dcb789be2d7451b6196b2
Marcos Cordeiro <marcos.cordeiro@andbank.com.br>
2021-12-07 22:04:11 -0300

    Fix cursor visibility not reseting

--- Wasari/Program.cs
+++ Wasari/Program.cs
@@ -20,52 +20,61 @@ namespace Wasari
     {
         private static async Task<int> Main(string[] args)
         {
-            Console.CursorVisible = false;
             var loggerConfiguration = new LoggerConfiguration();
 
             var konsoleAvailable =
-                RuntimeInformation.IsOSPlatform(OSPlatform.Windows) && KonsoleSink.AvailableHeight > 10 && args.All(i => i != "-nk");
+                RuntimeInformation.IsOSPlatform(OSPlatform.Windows) && KonsoleSink.AvailableHeight > 10 &&
+                args.All(i => i != "-nk");
 
-            loggerConfiguration = konsoleAvailable
-                ? loggerConfiguration.WriteTo.Sink<KonsoleSink>()
-                : loggerConfiguration.WriteTo.Console()
-                    .Filter
-                    .ByIncludingOnly(i =>
-                        i.Level != LogEventLevel.Information ||
-                        !i.MessageTemplate.Text.StartsWith("[Progress Update]"));
+            if (konsoleAvailable) Console.CursorVisible = false;
 
-            Log.Logger = loggerConfiguration
-                .WriteTo.File(
-                    Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
-                        "Wasari", "logs", "log.txt"), rollingInterval: RollingInterval.Day,
-                    restrictedToMinimumLevel: LogEventLevel.Verbose)
-                .CreateLogger();
-
-            if (!konsoleAvailable)
-                Log.Logger.Warning("Konsole isn't available, falling back to regular Console sink");
-            
-            var serviceCollection = new ServiceCollection();
-            serviceCollection.AddTransient<CrunchyRollAuthenticationService>();
-            serviceCollection.AddTransient<CrunchyrollDownloadSeriesCommand>();
-            serviceCollection.AddTransient<CrunchyrollListSeriesCommand>();
-            serviceCollection.AddLogging(c => c.AddSerilog());
-            serviceCollection.Configure<ProgressBarOptions>(o => o.Enabled = true);
-            await serviceCollection.AddEnvironmentServices();
-            await serviceCollection.AddCrunchyrollServices();
-
-            await using var serviceProvider = serviceCollection.BuildServiceProvider();
-            var environmentOptions = serviceProvider.GetService<IOptions<EnvironmentOptions>>();
-            if (environmentOptions?.Value?.Features is { } features)
+            try
             {
-                Log.Logger.Information("Available environment features: {@Features}", features);
-            }
+                loggerConfiguration = konsoleAvailable
+                    ? loggerConfiguration.WriteTo.Sink<KonsoleSink>()
+                    : loggerConfiguration.WriteTo.Console()
+                        .Filter
+                        .ByIncludingOnly(i =>
+                            i.Level != LogEventLevel.Information ||
+                            !i.MessageTemplate.Text.StartsWith("[Progress Update]"));
 
-            return await new CliApplicationBuilder()
-                .AddCommand<CrunchyrollDownloadSeriesCommand>()
-                .AddCommand<CrunchyrollListSeriesCommand>()
-                .UseTypeActivator(serviceProvider.GetService)
-                .Build()
-                .RunAsync(args.Where(i => i != "-nk").ToArray());
+                Log.Logger = loggerConfiguration
+                    .WriteTo.File(
+                        Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                            "Wasari", "logs", "log.txt"), rollingInterval: RollingInterval.Day,
+                        restrictedToMinimumLevel: LogEventLevel.Verbose)
+                    .CreateLogger();
+
+                if (!konsoleAvailable)
+                    Log.Logger.Warning("Konsole isn't available, falling back to regular Console sink");
+
+                var serviceCollection = new ServiceCollection();
+                serviceCollection.AddTransient<CrunchyRollAuthenticationService>();
+                serviceCollection.AddTransient<CrunchyrollDownloadSeriesCommand>();
+                serviceCollection.AddTransient<CrunchyrollListSeriesCommand>();
+                serviceCollection.AddLogging(c => c.AddSerilog());
+                serviceCollection.Configure<ProgressBarOptions>(o => o.Enabled = true);
+                await serviceCollection.AddEnvironmentServices();
+                await serviceCollection.AddCrunchyrollServices();
+
+                await using var serviceProvider = serviceCollection.BuildServiceProvider();
+                var environmentOptions = serviceProvider.GetService<IOptions<EnvironmentOptions>>();
+                if (environmentOptions?.Value?.Features is { } features)
+                {
+                    Log.Logger.Information("Available environment features: {@Features}", features);
+                }
+
+                return await new CliApplicationBuilder()
+                    .AddCommand<CrunchyrollDownloadSeriesCommand>()
+                    .AddCommand<CrunchyrollListSeriesCommand>()
+                    .UseTypeActivator(serviceProvider.GetService)
+                    .Build()
+                    .RunAsync(args.Where(i => i != "-nk").ToArray());
+            }
+            finally
+            {
+                if (konsoleAvailable) Console.CursorVisible = true;
+            }
         }
     }
 }
