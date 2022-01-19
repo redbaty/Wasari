e891210d7831f4e4b54b9719c9f19e1bb968665e
Marcos Cordeiro <marcos.cordeiro@andbank.com.br>
2022-01-19 13:03:31 -0300

    Adds beta support to crunchy-list

--- Wasari/Commands/CrunchyrollListSeriesCommand.cs
+++ Wasari/Commands/CrunchyrollListSeriesCommand.cs
@@ -1,9 +1,12 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using CliFx;
 using CliFx.Attributes;
 using CliFx.Infrastructure;
 using Wasari.Abstractions;
+using Wasari.Crunchyroll;
 using Wasari.Crunchyroll.Abstractions;
+using Wasari.Crunchyroll.API;
 
 namespace Wasari.Commands;
 
@@ -13,26 +16,39 @@ internal class CrunchyrollListSeriesCommand : ICommand
     [CommandParameter(0, Description = "Series URL.")]
     public string SeriesUrl { get; init; }
         
-    public CrunchyrollListSeriesCommand(ISeriesProvider<CrunchyrollSeasonsInfo> crunchyrollSeasonProvider)
+    public CrunchyrollListSeriesCommand(ISeriesProvider<CrunchyrollSeasonsInfo> crunchyrollSeasonProvider, CrunchyrollApiServiceFactory crunchyrollApiServiceFactory, BetaCrunchyrollService betaCrunchyrollService)
     {
         CrunchyrollSeasonProvider = crunchyrollSeasonProvider;
+        CrunchyrollApiServiceFactory = crunchyrollApiServiceFactory;
+        BetaCrunchyrollService = betaCrunchyrollService;
     }
+    
+    private CrunchyrollApiServiceFactory CrunchyrollApiServiceFactory { get; }
+    
+    private BetaCrunchyrollService BetaCrunchyrollService { get; }
 
     private ISeriesProvider<CrunchyrollSeasonsInfo> CrunchyrollSeasonProvider { get; }
         
     public async ValueTask ExecuteAsync(IConsole console)
     {
-        var seriesInfo = await CrunchyrollSeasonProvider.GetSeries(SeriesUrl);
-        await console.Output.WriteLineAsync(seriesInfo.Name);
-        
-        foreach (var crunchyrollSeasonsInfo in seriesInfo.Seasons)
-        {
-            await console.Output.WriteLineAsync($"* [{crunchyrollSeasonsInfo.Season:00}] {crunchyrollSeasonsInfo.Title}");
+        var isBeta = SeriesUrl.Contains("beta.");
+    
+        await CrunchyrollApiServiceFactory.CreateUnauthenticatedService();
 
-            foreach (var crunchyrollEpisodeInfo in crunchyrollSeasonsInfo.Episodes)
+        var seriesInfo = isBeta
+            ? await BetaCrunchyrollService.GetSeries(SeriesUrl)
+            : await CrunchyrollSeasonProvider.GetSeries(SeriesUrl);
+
+        foreach (var seasonsInfo in seriesInfo.Seasons)
+        {
+            await console.Output.WriteLineAsync($"S{seasonsInfo.Season:00} - {seasonsInfo.Title}");
+
+            foreach (var episode in seasonsInfo.Episodes)
             {
-                await console.Output.WriteLineAsync($"\t [{crunchyrollEpisodeInfo.Number}] {crunchyrollEpisodeInfo.Name}");
+                await console.Output.WriteLineAsync($"- {episode.Number}: {episode.Name}");
             }
+
+            await console.Output.WriteLineAsync();
         }
     }
 }
