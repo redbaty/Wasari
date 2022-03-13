0371492dc3c0d1ba9fbc9db4641c06fbbd768f4c
Marcos Cordeiro <marcos.cordeiro@andbank.com.br>
2022-03-13 20:42:36 -0300

    Adds legacy crunchyroll support on ProviderSolver

--- Wasari.App/SeriesProviderSolver.cs
+++ Wasari.App/SeriesProviderSolver.cs
@@ -11,6 +11,11 @@ public class SeriesProviderSolver
             return typeof(BetaCrunchyrollService);
         }
 
+        if (url.Host is "crunchyroll.com" or "www.crunchyroll.com")
+        {
+            return typeof(CrunchyrollService);
+        }
+
         throw new NotImplementedException("Failed to determine provider");
     }
 }
