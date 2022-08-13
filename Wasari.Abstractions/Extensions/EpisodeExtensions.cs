2c51873f4ba9b3a958bc10a8299489781732bce2
Marcos Cordeiro <mvmcjunior@gmail.com>
2022-08-13 01:13:00 -0300

    Move classes into its own file

--- Wasari.Abstractions/Extensions/EpisodeExtensions.cs
+++ Wasari.Abstractions/Extensions/EpisodeExtensions.cs
@@ -1,22 +1,6 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 
 namespace Wasari.Abstractions.Extensions;
-
-public static class DownloadParameterExtensions
-{
-    public static string FinalOutputDirectory(this DownloadParameters downloadParameters, string? seriesName)
-    {
-        var outputDirectory = downloadParameters.BaseOutputDirectory ?? Environment.CurrentDirectory;
-
-        if (downloadParameters.CreateSeriesFolder && !string.IsNullOrEmpty(seriesName))
-        {
-            outputDirectory = Path.Combine(outputDirectory, seriesName);
-        }
-
-        return outputDirectory;
-    }
-}
 
 public static class EpisodeExtensions
 {
