7b2f566e434ff3e0c22ad5e47714f9914b9a665b
Marcos Cordeiro <marcos.cordeiro@andbank-br.com.br>
2023-06-22 23:59:45 -0300

    Adds wasari daemon to solution

--- Wasari.App/Extensions/ApplicationExtensions.cs
+++ Wasari.App/Extensions/ApplicationExtensions.cs
@@ -1,5 +1,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using Wasari.App.Abstractions;
+using Wasari.FFmpeg;
+using Wasari.YoutubeDlp;
 
 namespace Wasari.App.Extensions;
 
@@ -20,6 +22,8 @@ public static class ApplicationExtensions
 
     public static IServiceCollection AddDownloadServices(this IServiceCollection serviceCollection)
     {
+        serviceCollection.AddFfmpegServices();
+        serviceCollection.AddYoutubeDlpServices();
         serviceCollection.AddScoped<GenericDownloadService>();
         serviceCollection.AddScoped<DownloadServiceSolver>();
         return serviceCollection;
