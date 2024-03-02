c10ff59f55f8583065b5b9466854e6167e4a8105
Marcos Cordeiro <marcos.cordeiro@andbank-br.com.br>
2024-03-02 17:19:03 -0300

    Adds EpisodeCachePolicy
    This allow us to also cache badrequests

---
+++ Wasari.Tvdb.Api/Policies/EpisodeCachePolicy.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.OutputCaching;
+
+namespace Wasari.Tvdb.Api.Policies;
+
+public class EpisodeCachePolicy : IOutputCachePolicy
+{
+    public static readonly EpisodeCachePolicy Instance = new();
+
+    private EpisodeCachePolicy()
+    {
+    }
+    
+    public ValueTask CacheRequestAsync(OutputCacheContext context, CancellationToken cancellation)
+    {
+        var attemptOutputCaching = AttemptOutputCaching();
+        context.EnableOutputCaching = true;
+        context.AllowCacheLookup = attemptOutputCaching;
+        context.AllowCacheStorage = attemptOutputCaching;
+        context.AllowLocking = true;
+        
+        var value = context.HttpContext.Request.Query["query"].ToString().Trim();
+        context.CacheVaryByRules.VaryByValues.Add(new KeyValuePair<string, string>("query", value));
+
+        context.ResponseExpirationTimeSpan = TimeSpan.FromMinutes(5);
+        return ValueTask.CompletedTask;
+    }
+
+    ValueTask IOutputCachePolicy.ServeFromCacheAsync
+        (OutputCacheContext context, CancellationToken cancellationToken)
+    {
+        return ValueTask.CompletedTask;
+    }
+
+    ValueTask IOutputCachePolicy.ServeResponseAsync
+        (OutputCacheContext context, CancellationToken cancellationToken)
+    {
+        var response = context.HttpContext.Response;
+        
+        if (response.StatusCode != StatusCodes.Status200OK && response.StatusCode != StatusCodes.Status400BadRequest)
+        {
+            context.AllowCacheStorage = false;
+            return ValueTask.CompletedTask;
+        }
+        
+        return ValueTask.CompletedTask;
+    }
+
+    private static bool AttemptOutputCaching() => true;
+}
--- Wasari.Tvdb.Api/Program.cs
+++ Wasari.Tvdb.Api/Program.cs
@@ -1,22 +1,20 @@
 using Wasari.Tvdb;
+using Wasari.Tvdb.Api.Policies;
 using Wasari.Tvdb.Api.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddResponseCaching();
 builder.Services.AddTvdbServices();
-builder.Services.AddOutputCache();
+builder.Services.AddOutputCache(options =>
+{
+    options.AddPolicy(nameof(EpisodeCachePolicy), EpisodeCachePolicy.Instance);
+});
 builder.Services.AddScoped<TvdbEpisodesService>();
 
 var app = builder.Build();
 app.UseOutputCache();
 app.MapGet("/episodes", (TvdbEpisodesService tvdbEpisodesService, string query) => tvdbEpisodesService.GetEpisodes(query))
-    .CacheOutput(o => o.Expire(TimeSpan.FromMinutes(5))
-        .SetVaryByQuery(Array.Empty<string>())
-        .VaryByValue(context =>
-        {
-            var value = context.Request.Query["query"].ToString().Trim();
-            return new KeyValuePair<string, string>("query", value);
-        }));
+    .CacheOutput(nameof(EpisodeCachePolicy));
 
 app.Run();
